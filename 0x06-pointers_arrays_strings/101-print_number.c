30b070389c12a58535a65ed67470ff82cfd7799a
Dare Oloruntoba <64743870+dretoba91@users.noreply.github.com>
2022-04-24 18:39:37 +0100

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,1 +1,35 @@
-#
+#include "main.h"
+
+/**
+ * print_number - prints an integer.
+ * @n: input integer.
+ */
+
+void print_number(int n)
+{
+unsigned int a, c, aux;
+
+if (n < 0)
+{
+_putchar(45);
+a = n * -1;
+}
+
+else
+{
+a = n;
+}
+c = a;
+aux = 1;
+
+while (c > 9)
+{
+c /= 10;
+aux *= 10;
+}
+
+for (; aux >= 1; aux /= 10)
+{
+_putchar(((a / aux) % 10) + 48);
+}
+}
