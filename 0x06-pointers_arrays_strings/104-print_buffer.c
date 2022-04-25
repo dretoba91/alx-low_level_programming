b28a9ee74b10724361baa1225c88ecfa79caaf31
Dare Oloruntoba <64743870+dretoba91@users.noreply.github.com>
2022-04-25 08:39:26 +0100

    Update 104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,14 +1,35 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_buffer - prints a buffer
- * @b: buffer.
- * @size: size of buffer.
- * Return: no return.
+ * print_number - printing number with puts
+ *
+ * @n: input int
  */
-void print_buffer(char *b, int size)
+void print_number(int n)
 {
+	/*Declaring variables*/
+	int count = 0, pow = 1;
+	unsigned int num = n;
 
 
+	if (n < 0) /*Evaluate this condition*/
+	{
+		_putchar('-');
+		num = -n;
+	}
+	while (n != 0)
+	{
+		n /= 10;
+		count++;
+	}
+	while (count > 1)
+	{
+		pow *= 10;
+		count--;
+	}
+	while (pow >= 1)
+	{
+		_putchar(num / pow % 10 + '0');
+		pow /= 10;
+	}
 }
