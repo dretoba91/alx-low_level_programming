4b409d1bb720751ede522c513dbfbe753fac5fb5
Dare Oloruntoba <64743870+dretoba91@users.noreply.github.com>
2022-05-31 05:40:51 +0100

    Update _putchar.c

--- 0x14-bit_manipulation/_putchar.c
+++ 0x14-bit_manipulation/_putchar.c
@@ -1,1 +1,13 @@
-#
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to display
+ * @c: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned.
+ */
+
+int _putchar(char c)
+{
+    return (write(1, &c, 1));
+}
