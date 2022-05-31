038876a855dedc2285b79aa63d5494cc448d5e66
Dare Oloruntoba <64743870+dretoba91@users.noreply.github.com>
2022-05-31 05:42:30 +0100

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,1 +1,17 @@
-#
+#include "main.h"
+
+/**
+ * print_binary - prints the binary equivalent of a decimal number
+ * @n: number to print in binary
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n < 2)
+		_putchar(n + '0');
+	else
+	{
+		print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+}
